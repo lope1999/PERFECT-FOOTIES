83f08a21046072192d29248a4ee4be6d3760eab2
lope1999 <corps.technical2.union@gmail.com>
2024-06-01 14:21:25 +0100

    Configured Database Connection with mysql, Created Authentication Module with JWT, Created Product Management Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configuration from './config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './security/auth/auth.module';
+import { UsersModule } from './security/users/users.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),   
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        ...configService.get('database'),
+        autoLoadEntities: true,
+      }),
+      inject: [ConfigService],
+    }),
+    AuthModule, 
+    UsersModule, ProductsModule
+  ],
+
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,39 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule,  { abortOnError: false });
-  await app.listen(3001);
+  const app = await NestFactory.create(AppModule);
+  //app.use(helmet());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+  app.enableCors();
+  app.setGlobalPrefix('api');
+
+  // Get required services
+  const configService = app.get(ConfigService);
+
+  // Open API
+  const swaggerConfig = new DocumentBuilder()
+    //.addBearerAuth()
+    .setTitle('perfect-footies')
+    .setDescription('Backend Apis for Project Perfect Footies')
+    .setVersion('1.0')
+    .addTag('api')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
+  // Websockets Gateway test (for sending messages to clients)
+  // const websocketsGateway = app.get(NotificationsGateway);
+  // setInterval(() => websocketsGateway.sendMessage(), 2000);
+
+  await app.listen(configService.get<string>('port'));
 }
+
 bootstrap();
